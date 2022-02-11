fbeb59773f5b3d256c720c7e848add1a1b24c87e
Lejeune <theolejeune.tl@gmail.com>
2022-02-11 16:08:20 +0100

    nik Unity

---
+++ Application Unlock/Assets/Scripts/Theo/Codes.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Codes : MonoBehaviour
+{
+    [SerializeField] private List<int> codes = new List<int>();
+
+
+    
+
+}
