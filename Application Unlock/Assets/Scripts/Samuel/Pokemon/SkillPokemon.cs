afd18fd424e54568bbe3cdb93f19e66ac945db48
iesnihS <fullcandies@gmail.com>
2022-02-16 14:15:02 +0100

    Oui enfin j'imagine

---
+++ Application Unlock/Assets/Scripts/Samuel/Pokemon/SkillPokemon.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class SkillPokemon
+{
+    [SerializeField] private string NameOfSkill;
+    [SerializeField] private Sprite IconeOfSkill;
+}
--- Application Unlock/Assets/Scripts/Samuel/Pokemon/UI/LogInterfacePokemon.cs
+++ Application Unlock/Assets/Scripts/Samuel/Pokemon/UI/LogInterfacePokemon.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class LogInterfacePokemon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Image PokemonIcone;
+    [SerializeField] private Image Type1;
+    [SerializeField] private Image Type2;
+    [SerializeField] private TextMeshProUGUI tmpro;
 
-    // Update is called once per frame
-    void Update()
+    private PokemonData CurrentPokemon;
+
+    public PokemonData currentPokemon => CurrentPokemon;
+    private bool HaveAPokemon;
+
+    public bool haveAPokemon => HaveAPokemon;
+    public void ComputeLogInterface(Sprite pokemonIcone, Sprite type1, Sprite type2, string name, PokemonData pokemon)
     {
-        
+        PokemonIcone.sprite = pokemonIcone;
+        Type1.sprite = type1;
+        Type2.sprite = type2;
+        tmpro.text = name;
+        HaveAPokemon = true;
+        CurrentPokemon = pokemon;
     }
 }
