9741bd219590ed514051deb909ddb63fe38a7a5c
Nutshell01 <92015705+Nutshell01@users.noreply.github.com>
2022-03-18 21:25:13 +0100

    lampe sale est faite, ainsi que les indices ma couille

---
+++ Application Unlock/Assets/Scripts/Enzo/Lampe/LampeSale.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class LampeSale : MonoBehaviour
+{
+    [SerializeField] float _cleanness = 100;
+    [SerializeField] float _cleaningCoefficiency;
+    [SerializeField] Button _lampButton;
+    private bool _isClean;
+
+     #region RéponseUI
+    [SerializeField] GameObject _answerBox;
+    [SerializeField] TextMeshProUGUI _answerText;
+    [SerializeField] string _wrongText;
+    [SerializeField] string _textToChange;
+    
+    #endregion
+   
+    void Update()
+    {    
+         
+        if(_cleanness <= 0)
+        {
+            Victory();
+            _cleanness = 100;
+        }
+
+    }
+
+    public void OnMouseDrag()
+    {
+        _cleanness -= _cleaningCoefficiency * Time.deltaTime;
+    }
+    public void Victory()
+    {
+        _lampButton.enabled = false;
+        _answerBox.SetActive(true);
+        _answerText.text = _textToChange;
+        
+    }
+}
