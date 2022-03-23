550d11fc2a85bae148a5ca52c5369e93a7c5076c
iesnihS <fullcandies@gmail.com>
2022-03-23 11:03:11 +0100

    Update build.bat

--- Application Unlock/build.bat
+++ Application Unlock/build.bat
@@ -1,7 +1,7 @@
 set unity=C:\Program Files\2020.3.20f1\Editor\Unity.exe
-set butler=C:\Users\Suzan\Desktop\Butler
+set butler=C:\Users\Suzan\Desktop\Butler\butler.exe
 git pull
 "%unity%" -quit -batchmode -executeMethod BuildWebGL.Build
 cd Builds
 tar -cvzf "WebGL.zip" "WebGL"
-"%butler%" push "WebGL.zip" iesnihS/pokescape:html ~~userversion 0.1
+"%butler%" push "WebGL.zip" iesnihS/pokescape:html --userversion 0.1
