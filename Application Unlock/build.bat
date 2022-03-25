fefeafc7d193e411c6cf84eb6aee13b7708a93ee
iesnihS <fullcandies@gmail.com>
2022-03-25 13:59:51 +0100

    Build Bat

--- Application Unlock/build.bat
+++ Application Unlock/build.bat
@@ -3,5 +3,5 @@ set butler=C:\Users\Suzan\Desktop\Butler\butler.exe
 git pull
 "%unity%" -quit -batchmode -executeMethod BuildWebGL.Build
 cd Builds
-tar -cvzf "WebGL.zip" "WebGL"
-"%butler%" push "WebGL.zip" iesnihS/pokescape:html --userversion 0.1
+tar -a -c -f "WebGL.zip" "WebGL"
+"%butler%" push "WebGL.zip" darkbuilder646/pokescape:html --userversion 0.1
