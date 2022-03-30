b176ced82abb89259b8850bef6b935f1915c3565
iesnihS <fullcandies@gmail.com>
2022-03-30 10:14:37 +0200

    rtt'

--- Application Unlock/Assets/Scripts/Theo/Microphone/AudioMic.cs
+++ Application Unlock/Assets/Scripts/Theo/Microphone/AudioMic.cs
@@ -20,8 +20,9 @@ public class AudioMic : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     public void OnPointerUp(PointerEventData eventData)
     {
         //End the recording when the mouse comes back up, then play it
+#if !UNITY_WEBGL ||UNITY_EDITOR
         Microphone.End("");
-
+#endif
         //Trim the audioclip by the length of the recording
         AudioClip recordingNew = AudioClip.Create(recording.name, (int)((Time.time - startRecordingTime) * recording.frequency), recording.channels, recording.frequency, false);
         float[] data = new float[(int)((Time.time - startRecordingTime) * recording.frequency)];
@@ -41,15 +42,19 @@ public class AudioMic : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     public void OnPointerDown(PointerEventData eventData)
     {
         //Get the max frequency of a microphone, if it's less than 44100 record at the max frequency, else record at 44100
-        int minFreq;
-        int maxFreq;
+        int minFreq = 0;
+        int maxFreq = 44100;
         int freq = 44100;
+#if !UNITY_WEBGL || UNITY_EDITOR
         Microphone.GetDeviceCaps("", out minFreq, out maxFreq);
+#endif
         if (maxFreq < 44100)
             freq = maxFreq;
 
         //Start the recording, the length of 300 gives it a cap of 5 minutes
+#if !UNITY_WEBGL || UNITY_EDITOR
         recording = Microphone.Start("", false, 300, 44100);
+#endif
         startRecordingTime = Time.time;
     }
 
