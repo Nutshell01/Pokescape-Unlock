3b310cdb87cc776fc264f8d8fe1a5890fe2d6ac5
PikaLorie <lorie.rousselle@gmail.com>
2022-04-05 22:36:06 +0200

    Psytigri qui pete sa biere

--- Application Unlock/Assets/Scripts/Lorie/PokemonCatch/SpawnPokemon.cs
+++ Application Unlock/Assets/Scripts/Lorie/PokemonCatch/SpawnPokemon.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class SpawnPokemon : MonoBehaviour
 {
-    public PokemonData pokemonData;
     private Vector3 toto = new Vector3(0,-0.7f,4);
+   
 
-    private void Start()
+
+    public void Poke()
     {
-       GameObject.Instantiate(pokemonData.pokemonModel3D, toto, pokemonData.pokemonModel3D.transform.rotation);
+        //GameObject.Instantiate(pokemonData.pokemonModel3D, toto, pokemonData.pokemonModel3D.transform.rotation);
+        //GameObject.Instantiate(pokemonData.pokemonModel3D, toto, pokemonData.pokemonModel3D.transform.rotation);
     }
 
- 
+   
+
 }
