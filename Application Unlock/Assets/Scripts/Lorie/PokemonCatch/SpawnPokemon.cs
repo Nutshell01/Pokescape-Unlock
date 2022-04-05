0cb64740306599a8f86e0c5f5d1048b852ad76a0
PikaLorie <lorie.rousselle@gmail.com>
2022-04-05 20:47:25 +0200

    Ouais

--- Application Unlock/Assets/Scripts/Lorie/PokemonCatch/SpawnPokemon.cs
+++ Application Unlock/Assets/Scripts/Lorie/PokemonCatch/SpawnPokemon.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class SpawnPokemon : MonoBehaviour
 {
     public PokemonData pokemonData;
+    private Vector3 toto = new Vector3(0,-0.7f,4);
 
     private void Start()
     {
-        GameObject.Instantiate(pokemonData.pokemonModel3D, Vector3.zero, pokemonData.pokemonModel3D.transform.rotation, transform);
+       GameObject.Instantiate(pokemonData.pokemonModel3D, toto, pokemonData.pokemonModel3D.transform.rotation);
     }
 
  
