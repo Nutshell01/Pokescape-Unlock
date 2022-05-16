adb6fa4029eb12a0cd221e24cc3340b170312a27
Nutshell01 <92015705+Nutshell01@users.noreply.github.com>
2022-05-16 11:55:49 +0200

    fix on the flux

--- Application Unlock/Assets/Scripts/Enzo/Coffre/OpenChest.cs
+++ Application Unlock/Assets/Scripts/Enzo/Coffre/OpenChest.cs
@@ -45,34 +45,43 @@ public class OpenChest : MonoBehaviour
 
     #endregion
 
+    [SerializeField] GameManager _gameManager;
+    [SerializeField] StarterManager starterManager;
+
     void OnGUI()
     {
         _inputChest.text = Regex.Replace(_inputChest.text, @"[^0-9 ]", "");
     }
     // Start is called before the first frame update
-    void Start()
+  
+    // Update is called once per frame
+    void Update()
     {
-        if (_isPsytigri)
+        //check quel starter (a faire dans une fonction pour eviter de devoir le faire en boucle ??)
+
+        if (starterManager.currentStarter.name == "Psytigri")
+        {
+            _isPsytigri = true;
+        }
+        else if (starterManager.currentStarter.name == "Bagguiguane")
+        {
+            _isPsytigri = false;
+        }
+
+        if (_isPsytigri == true)
         {
             _clavierKey.SetActive(true);
             _buttonChest.enabled = false;
             _buttonKey.SetActive(true);
 
         }
-        else
+        else if (_isPsytigri == false)
         {
             _clavierKey.SetActive(false);
             _buttonChest.enabled = true;
             _buttonKey.SetActive(false);
 
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //check quel starter (a faire dans une fonction pour eviter de devoir le faire en boucle ??)
-
 
         if (_currentCount == _countToDestroy)
         {
